915e4b167015064e5446c8838ba470311c19ff6f
sooani <118452650+sooani@users.noreply.github.com>
2023-11-08 20:30:41 +0900

    [level unrated] Title: 문자열 돌리기, Time: 177.16 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -4,11 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        if( a.length() >= 1 && a.length() <= 10){
-            for ( int i = 0; i < a.length(); i++){
-            char b = a.charAt(i);
-            System.out.printf("%s\n",b);
-            }
+            for ( char ch : a.toCharArray()) {
+                System.out.println(ch);
         }
     }
 }
