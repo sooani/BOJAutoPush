4ac560b45aca7fca8996de27e93d1e9b3e782114
sooani <118452650+sooani@users.noreply.github.com>
2023-12-09 20:48:37 +0900

    [level unrated] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        myString = myString.toUpperCase();
-        return myString;
+        return myString.toUpperCase();
     }
 }
