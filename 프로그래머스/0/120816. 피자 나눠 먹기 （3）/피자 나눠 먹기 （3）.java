796cd9d79be9112d490a1c5ea60438f74b4e9c99
sooani <118452650+sooani@users.noreply.github.com>
2024-01-31 13:00:08 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int slice, int n) {
-        int result = (n % slice == 0) ? n/slice : n/slice+1;
-        return result;
+        return (n % slice == 0) ? n/slice : n/slice+1;
     }
 }
