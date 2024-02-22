b80302b3f6c871fdf47a42d870d7c969c79fa0aa
sooani <118452650+sooani@users.noreply.github.com>
2024-02-22 17:32:21 +0900

    [level 0] Title: 정수 찾기, Time: 1.17 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,7 +1,7 @@
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] num_list, int n) {
-        return Arrays.stream(num_list).anyMatch(num -> num == n) ? 1 : 0;
+        return IntStream.of(num_list).anyMatch(num -> num == n) ? 1 : 0;
     }
 }
