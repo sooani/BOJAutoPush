a0f8e3865db3f3e536d6acc4833953003d7f0606
sooani <118452650+sooani@users.noreply.github.com>
2024-04-19 23:52:29 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.96 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = 2 * i + 1;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
