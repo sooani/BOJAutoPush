5c9fa6dce4c31ce2acfc17da5d1a060991b3eba4
sooani <118452650+sooani@users.noreply.github.com>
2024-04-22 03:24:16 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.28 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.Collectors;
+class Solution {
+    public int[] solution(int[] num_list) {
+        List<Integer> list = Arrays.stream(num_list)
+            .boxed().collect(Collectors.toList());
+         
+        Collections.reverse(list);
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
