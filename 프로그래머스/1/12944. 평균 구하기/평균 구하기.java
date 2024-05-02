11f6d0314cdb2d196bf106e6bccce7eb3b4eb2ae
sooani <118452650+sooani@users.noreply.github.com>
2024-05-02 20:30:32 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int count = 0;
+        for(int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+            count++;
+        }
+        return answer / count;
+    }
+}
