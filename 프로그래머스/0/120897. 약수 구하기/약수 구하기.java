4562ac33dae3824a87aa46f5f358ac2883c0a2b3
sooani <118452650+sooani@users.noreply.github.com>
2024-05-02 22:58:10 +0900

    [level 0] Title: 약수 구하기, Time: 2.84 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -4,12 +4,11 @@ class Solution {
     public int[] solution(int n) {
         ArrayList<Integer> num = new ArrayList<>();
         
-        for( int i = 1; i <= n / 2; i++) {
+        for( int i = 1; i <= n; i++) {
             if(n % i == 0){
             num.add(i);
                 }
         }
-        num.add(n);
         return num.stream().mapToInt(Integer::intValue).toArray();
     }
 }
