4b3324159140fd8c1bfdd833698fb3e17d6f9bdc
sooani <118452650+sooani@users.noreply.github.com>
2024-05-02 20:26:42 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int num = 0;
+        while (n > 0) {
+        num += n % 10;
+        n /= 10;
+        }
+        return num;
+    }
+}
