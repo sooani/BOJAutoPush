aff91cdf3b4486180d5d7cc44f15df4da0df489b
sooani <118452650+sooani@users.noreply.github.com>
2024-05-08 04:01:41 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.94 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        int[] result = my_string.chars()
+            .filter(Character::isDigit)
+            .map(Character::getNumericValue)
+            .sorted()
+            .toArray();
+        
+        return result;
+    }
+}
