55fec48afc85b4d6f38f5b3ec7a87e1c121aacd8
sooani <118452650+sooani@users.noreply.github.com>
2024-05-23 02:38:08 +0900

    [level 0] Title: 세로 읽기, Time: 9.28 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,15 +1,9 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        StringBuilder answer = new StringBuilder();
-        
-        for(int i = 0; i <= my_string.length(); i += m){
-            int end = Math.min(i + m , my_string.length());
-            String sub = my_string.substring(i, end);
-            
-            if(sub.length() >= c){
-                answer.append(sub.charAt(c-1));
-            }
+        String answer = "";
+        for(int i = c - 1; i < my_string.length(); i += m){
+            answer += my_string.charAt(i);
         }
-        return answer.toString();
+        return answer;
     }
 }
