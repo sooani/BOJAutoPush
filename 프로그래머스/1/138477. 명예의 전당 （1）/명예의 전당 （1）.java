a6f3b7024a68d54f8deda9226218f79fd9aefc18
sooani <118452650+sooani@users.noreply.github.com>
2024-09-17 01:33:19 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.35 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,19 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        // 리스트 점수에 k개가 초과하면 작은 값 지우고 새로운 값 업데이트
-        int[] answer = new int[score.length]; // 결과
-        List<Integer> showList = new ArrayList<>();
+        int[] answer = new int[score.length];
+        // 우선순위 큐
+        PriorityQueue<Integer> showList = new PriorityQueue<>();
 
         for (int i = 0; i < score.length; i++) {
-            showList.add(score[i]);
-
+            showList.offer(score[i]);
+            
             if (showList.size() > k) {
-                showList.remove(Collections.min(showList)); 
-                // k개 초과하면 가장 작은 값 제거
+                showList.poll(); // 가장 작은 값을 제거
             }
-            answer[i] = Collections.min(showList);
-            // 리스트에서 가장 작은 값 저장
+            answer[i] = showList.peek(); // 큐의 맨 앞 값 저장
         }
         return answer;
     }
