fb0141ee06de4125e21bbb269b8801b04fce8dd5
sooani <118452650+sooani@users.noreply.github.com>
2024-10-12 01:06:27 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 160 ms, Memory: 17468 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        
+        switch (input) {
+            case "M":
+                System.out.println("MatKor");
+                break;
+            case "W":
+                System.out.println("WiCys");
+                break;
+            case "C":
+                System.out.println("CyKor");
+                break;
+            case "A":
+                System.out.println("AlKor");
+                break;
+            case "$":
+                System.out.println("$clear");
+                break;
+            default:
+                System.out.println("Invalid input.");
+                break;
+        }
+    }
+}
