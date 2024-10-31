4d9a6f8fc3ccc2f4ce460f9de892c611b23c07f7
sooani <118452650+sooani@users.noreply.github.com>
2024-11-01 00:49:47 +0900

    [Bronze V] Title: 세금, Time: 192 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.print((int)(N * 0.78)+ " ");
+        System.out.print((int)(N * 0.8 + N * 0.2 * 0.78));
+        sc.close();
+    }
+}
