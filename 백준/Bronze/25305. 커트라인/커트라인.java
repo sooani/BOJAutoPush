dd53dbf62b8299f8b1db407fe49d11d8677b4fb9
sooani <118452650+sooani@users.noreply.github.com>
2024-11-13 02:34:29 +0900

    [Bronze II] Title: 커트라인, Time: 212 ms, Memory: 19448 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+        int[] score = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            score[i] = sc.nextInt();
+        }
+        Arrays.sort(score);
+        System.out.println(score[N - k]);
+    }
+}
